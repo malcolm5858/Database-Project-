2ac6b57f95da2fe1316ebfd0bf7c91d9e9ccd8f0
Malcolm Machesky <Malcolm9292@gmail.com>
2020-04-23 21:13:14 -0400

    Commit 2

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
 import java.sql.*;
 
 public static void main(String[] args) {
+    String user = "postgres";
+    String password ="1234";
+
+    try{
+                 Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/pstgres", user, password);
+                 System.out.println("Connected to the PostgreSQL server successfully."); 
+    		 	 
+    	 }catch (SQLException e) {
+                  System.out.println(e.getMessage());
+         }
     
 }
